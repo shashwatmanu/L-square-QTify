87a0a097f07ad669323c8b7cf10f0cdd11a6bff7
shashwatmanu <shashwatmanu@outlook.com>
2024-04-23 16:17:30 +0530

    Navbar commit

--- qtify/src/Components/Logo/Logo.jsx
+++ qtify/src/Components/Logo/Logo.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import LogoImage from "../../Assets/Logo.png";
 
 const Logo = () => {
-    return <img src={LogoImage} alt="logo"/>
+    return <img src={LogoImage} alt="logo" style={{height: "34px"}}/>
 }
 export default Logo;
