c1000e54c9b09b6532b6cc2d15d72589cc4b39cd
shashwatmanu <shashwatmanu@outlook.com>
2024-04-28 23:55:11 +0530

    Albums commit

---
+++ qtify/src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Scrollbar } from 'swiper/modules';
+import Card  from '../../Components/Card/CardComponent'
+import 'swiper/css';
+import Box from '@mui/material/Box';
+import './Carousel.css'
+import 'swiper/css/navigation';
+import { useRef } from 'react';
+import nextImage from "../../Assets/NextButton.png"
+import prevImage from "../../Assets/BackButton.png"
+
+const Carousel = ({data}) => {
+    const prevRef = useRef(null);
+  const nextRef = useRef(null);
+
+  return (
+    <div className='carousel'>
+         <Swiper
+      spaceBetween={50}
+      modules={[Navigation, Scrollbar]}
+    //   navigation={{
+    //     prevEl: prevRef.current,
+    //     nextEl: nextRef.current,
+    //   }}
+    navigation
+    //   navigation={{ nextEl: "swiper-button-next", prevEl: "swiper-button-prev" }}
+    
+      scrollbar={{ draggable: true }}
+      slidesPerView={'auto'}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+        
+        {data.map((album)=>{ return(
+            <SwiperSlide className='swiper-slide'>
+            <Card img={album.image} follows={album.follows} albumName={album.title}  key={album.id}/>
+            </SwiperSlide>)
+        })}
+        
+        {/* <SwiperSlide><Card/></SwiperSlide> */}
+        {/* <button ref={prevRef}><img src={prevImage}/></button>
+      <button ref={nextRef}><img src={nextImage}/></button> */}
+
+    </Swiper>
+
+    </div>
+  )
+}
+export default Carousel;
--- qtify/src/Components/Section/Section.jsx
+++ qtify/src/Components/Section/Section.jsx
@@ -4,24 +4,31 @@ import Box from '@mui/material/Box';
 import "./Section.css"
 import Grid from '@mui/material/Grid';
 import { Button } from '@mui/material';
+import { useState } from 'react';
+import  Carousel  from '../Carousel/Carousel';
 
 const Section = ({albumData, sectionName}) => {
+  const [showAll, setShowAll] = useState(false);
   const gridLayout = albumData.map((album)=>
   <Grid item>
     <Card img={album.image} follows={album.follows} albumName={album.title} key={album.id}/>
     </Grid>
   )
+ const handleClick = () => {
+  setShowAll(!showAll);
+ }
+
   return (<>
    <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginLeft: '32px', marginRight: '30px', marginBottom: '12px'}}>
 <Box sx={{color: 'white'}}>{sectionName}</Box>
-<Box><button className='button'>Collapse</button></Box>
+<Box sx={{marginBottom:'10px'}}><button className='button' onClick={handleClick}>{showAll?"Collapse":"Show all"}</button></Box>
    </Box>
-  <Box sx={{marginLeft: '32px', marginRight: '32px', marginBottom: '32px'}}>
+  {showAll?(<><Box sx={{marginLeft: '32px', marginRight: '32px', marginBottom: '32px'}}>
     <Grid container spacing={4}>
       {gridLayout}
       </Grid>
       </Box>
-      <hr/>
+      <hr/></>):(<Carousel data={albumData}/>)}
     </>
   )
 }
